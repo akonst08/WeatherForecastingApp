069813ab4639a466549dd041d755ac0025bd88cf
akonst08 <akonst08@ucy.ac.cy>
2026-04-24 20:01:06 +0300

    Update config.php
    Removed instructions for copying the config file.

--- backend/php/config.php
+++ backend/php/config.php
@@ -1,5 +1,5 @@
 <?php
-// Copy this file to config.php and fill your real database credentials.
+
 define('DB_HOST', 'sql203.infinityfree.com');
 define('DB_USER', 'if0_41745831');
 define('DB_PASS', 'MY_VPANEL_PASSWORD');
