01ff07b6029bbde4619636aca801c707740fc73e
akonst08 <akonst08@ucy.ac.cy>
2026-04-24 20:00:01 +0300

    Update config.example.php

--- backend/php/config.example.php
+++ backend/php/config.example.php
@@ -2,5 +2,5 @@
 // Copy this file to config.php and fill your real database credentials.
 define('DB_HOST', 'sql203.infinityfree.com');
 define('DB_USER', 'if0_41745831');
-define('DB_PASS', 'YOUR_VPANEL_PASSWORD');
+define('DB_PASS', 'MY_VPANEL_PASSWORD');
 define('DB_NAME', 'if0_41745831_weatherapp');
